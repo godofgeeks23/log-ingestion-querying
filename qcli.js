f9087fc8c8d48c5528490e7de4ab275b126691c1
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-11-23 22:07:25 +0530

    Update qcli.js

--- qcli.js
+++ qcli.js
@@ -6,8 +6,14 @@ program
   .option('--first')
   .option('-s, --separator <char>');
 
+// This function parses the command-line arguments and extracts the values for the defined options.
 program.parse();
 
+
+
+// used to retrieve the values of the parsed options. The returned value is stored in the options variable.
 const options = program.opts();
+
 const limit = options.first ? 1 : undefined;
+
 console.log(program.args[0].split(options.separator, limit));
