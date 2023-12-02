57e00ec84c82c886724a95be7a53d114bda9833b
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-12-02 23:10:34 +0530

    Update qcli.js

--- qcli.js
+++ qcli.js
@@ -10,12 +10,9 @@ program
 program.parse();
 
 
-
 // used to retrieve the values of the parsed options. The returned value is stored in the options variable.
 const options = program.opts();
 
 const limit = options.first ? 1 : undefined;
 
 console.log(program.args[0].split(options.separator, limit));
-
-// end of qcli.js
